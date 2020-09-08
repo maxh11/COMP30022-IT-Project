feab971130d1c3e942a6d9048a9acdcf36ee1c7c
maxh11 <mhollands@student.unimelb.edu.au>
2020-09-08 14:24:52 +1000

    removed useless shit

---
+++ eportfolio/src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+export default App;
+
+/*
+---- could have also written this ----
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/components/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
--- eportfolio/src/index.js
+++ eportfolio/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Car {
+    constructor(name) {
+        this.brand = name;
+    }
+    present = () => 'I have a ' + this.brand;
+}
+var mycar = new Car("Tesla");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const myfirstelement = <h1>{mycar.present()}</h1>
+ReactDOM.render(myfirstelement, document.getElementById('root'));
